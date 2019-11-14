b682fc907b8b8f5bb1366cc9cefe7ba1bc79661e
Frank <fmarinellicollege@gmail.com>
2019-11-14 06:30:28 -0500

    First 3 steps to Networking Assignment

---
+++ Assignment12/Assignment12-FrankM/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,12 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        GameObject playerToSpawn = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        playerToSpawn.GetComponent<Player>().color = new Color(Random.RandomRange(0.0f, 1.0f), Random.RandomRange(0.0f, 1.0f), Random.RandomRange(0.0f, 1.0f));
+        NetworkServer.AddPlayerForConnection(conn, playerToSpawn);
+    }
+}
