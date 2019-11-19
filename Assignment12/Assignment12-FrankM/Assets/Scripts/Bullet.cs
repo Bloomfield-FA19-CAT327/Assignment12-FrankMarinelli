9e734ff87fa272a7a909fc6f3b9b76a2f84c1054
Frank <fmarinellicollege@gmail.com>
2019-11-19 15:25:37 -0500

    Completed Step 9

--- Assignment12/Assignment12-FrankM/Assets/Scripts/Bullet.cs
+++ Assignment12/Assignment12-FrankM/Assets/Scripts/Bullet.cs
@@ -12,4 +12,9 @@ public class Bullet : NetworkBehaviour
     {
         gameObject.GetComponent<Renderer>().material.color = color;
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
 }
